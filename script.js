4fcf6c3ac54f1d8dab7d39d4bbf871f11caca7bb
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-07-16 17:05:05 +0600

    added smoke class to cerate smoke when asteroid touches earth, added earth class to rotate at the bottom

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     const canvas = document.getElementById("canvas1")
     const ctx = canvas.getContext("2d")
     canvas.width = 600
@@ -7,13 +7,48 @@ window.addEventListener("load", function(){
     ctx.strokeStyle = "white"
     ctx.lineWidth = 3
 
+    class Earth {
+        constructor(game, x, y) {
+            this.game = game
+            this.x = x
+            this.y = y
+            this.radius = 440
 
-    class Asteroid{
-        constructor(game){
+            this.image = document.getElementById("earth")
+            this.spriteWidth = 1000
+            this.spriteHeight = 1000
+            this.width = this.spriteWidth
+            this.height = this.spriteHeight
+            this.angle = 0
+            this.va = 0.001
+        }
+
+        draw(context) {
+            context.save()
+            context.translate(this.x, this.y)
+            context.rotate(this.angle)
+
+            context.drawImage(this.image, -this.spriteWidth * 0.5, -this.spriteHeight * 0.5, this.width, this.height)
+            context.restore()
+            // if(this.game.debug){
+            context.beginPath()
+            context.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+            context.stroke()
+            // }
+        }
+
+        update() {
+            this.angle += this.va
+        }
+    }
+
+
+    class Asteroid {
+        constructor(game) {
             this.game = game
             this.radius = 75
             this.x = Math.random() * this.game.width
-            this.y = -this.radius 
+            this.y = -this.radius
             this.image = document.getElementById("asteroid")
             this.spriteWidth = 150
             this.spriteHeight = 155
@@ -22,8 +57,8 @@ window.addEventListener("load", function(){
             this.angle = 0
             this.va = Math.random() * 0.02 - 0.01
         }
-        draw(context){
-            if(!this.free){
+        draw(context) {
+            if (!this.free) {
                 // context.beginPath()
                 // context.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
                 // context.stroke()
@@ -35,39 +70,89 @@ window.addEventListener("load", function(){
                 context.restore()
             }
         }
-        update(){
+        update() {
             this.angle += this.va
-            if(!this.free){
+            if (!this.free) {
                 this.y += this.speed
-                if(this.y > this.game.height + this.radius){
+                if (this.y > this.game.height + this.radius) {
                     this.reset()
                 }
 
-                // check collision 
-
-                if (this.y > 600){
-                    this.game.explosion.push(new Explosion(this.game, this.x, this.y))
-                    this.reset()
+                // check collision with player
+                if (this.y > 300) {
+                    this.blast()
                 }
 
-
+                
             }
-
         }
 
-        reset(){
-            this.free = true 
+        blast() {
+            this.game.explosion.push(new Explosion(this.game, this.x, this.y))
+            this.reset()
         }
 
-        start(){
+        reset() {
+            this.free = true
+        }
+
+        start() {
             this.free = false
             this.x = Math.random() * this.game.width
             this.y = -this.radius
         }
     }
 
-    class Explosion{
-        constructor(game, x, y){
+    class Smoke {
+        constructor(game, x, y) {
+            this.game = game
+            this.x = x
+            this.y = y
+
+            this.image = document.getElementById("smoke")
+            this.spriteWidth = 171
+            this.spriteHeight = 250
+            this.spriteX = 0
+            this.spriteY = 0
+            this.frameX = this.x - this.spriteWidth * 0.5
+            this.frameY = this.y - this.spriteHeight * 0.5
+
+            this.timer = 0
+            this.frame = 7
+            this.interval = 500 / this.frame
+
+            this.finished = false
+
+        }
+
+        draw(context) {
+            if (this.game.debug) {
+                context.beginPath()
+                context.arc(this.x, this.y, 100, 0, Math.PI * 2)
+                context.fill()
+            }
+
+            context.drawImage(this.image, this.spriteX * this.spriteWidth, this.spriteY * this.spriteWidth, this.spriteWidth, this.spriteHeight, this.frameX, this.frameY, this.spriteWidth, this.spriteHeight)
+        }
+
+        update(deltaTime) {
+            if (this.timer > this.interval) {
+                if (this.spriteX < this.frame - 1) {
+                    this.spriteX++
+                } else {
+                    this.finished = true
+                    this.game.explosion = this.game.explosion.filter(exp => !exp.finished)
+                }
+                this.timer = 0
+            } else {
+                this.timer += deltaTime
+            }
+        }
+    }
+
+
+    class Explosion {
+        constructor(game, x, y) {
             this.game = game
             this.x = x
             this.y = y
@@ -82,14 +167,14 @@ window.addEventListener("load", function(){
 
             this.timer = 0
             this.frame = 48
-            this.interval = 1000/ this.frame
+            this.interval = 1000 / this.frame
 
             this.finished = false
 
         }
 
-        draw(context){
-            if (this.game.degub){
+        draw(context) {
+            if (this.game.degub) {
                 context.beginPath()
                 context.arc(this.x, this.y, 100, 0, Math.PI * 2)
                 context.fill()
@@ -97,15 +182,15 @@ window.addEventListener("load", function(){
 
             context.drawImage(this.image, this.spriteX * this.spriteWidth, this.spriteY * this.spriteWidth, this.spriteWidth, this.spriteHeight, this.frameX, this.frameY, this.spriteWidth, this.spriteHeight)
         }
-        
-        update(deltaTime){
-            if (this.timer > this.interval){
-                if (this.spriteX < 7){
-                    this.spriteX ++
+
+        update(deltaTime) {
+            if (this.timer > this.interval) {
+                if (this.spriteX < 7) {
+                    this.spriteX++
                 } else if (this.spriteY < 5) {
-                    this.spriteY ++
-                    this.spriteX = 0 
-                } else{
+                    this.spriteY++
+                    this.spriteX = 0
+                } else {
                     this.finished = true
                     this.game.explosion = this.game.explosion.filter(exp => !exp.finished)
                 }
@@ -116,8 +201,9 @@ window.addEventListener("load", function(){
         }
     }
 
-    class Game{
-        constructor(width, height){
+
+    class Game {
+        constructor(width, height) {
             this.width = width
             this.height = height
             this.asteroidPool = []
@@ -125,29 +211,41 @@ window.addEventListener("load", function(){
             this.asteroidTimer = 0
             this.asteroidInterval = 1000
 
+            this.earth = new Earth(this, this.width * 0.5, this.height + 200)
+
             this.debug = false
 
-            this.explosion = []
+            this.explosion = [new Smoke(this, 300, 400)]
             this.createAsteroidPool()
         }
 
-        createAsteroidPool(){
-            for(let i = 0; i < this.max; i++){
+        createAsteroidPool() {
+            for (let i = 0; i < this.max; i++) {
                 this.asteroidPool.push(new Asteroid(this))
             }
         }
 
-        getElement(){
-            for (let i = 0; i < this.asteroidPool.length; i++){
-                if (this.asteroidPool[i].free){
+        getElement() {
+            for (let i = 0; i < this.asteroidPool.length; i++) {
+                if (this.asteroidPool[i].free) {
                     return this.asteroidPool[i]
                 }
             }
         }
 
-        render(context, deltaTime){
+        checkCollision(a, b){
+            const dx = a.x - b.x
+            const dy = a.y = b.y
+
+            const distance = Math.hypot(dy, dx)
+            const sumOfRadii = a.radius + b.radius
+
+            return [(distance < sumOfRadii), distance, sumOfRadii, dx, dy]
+        }
+
+        render(context, deltaTime) {
             // create asteroid periodically 
-            if (this.asteroidTimer > this.asteroidInterval){
+            if (this.asteroidTimer > this.asteroidInterval) {
                 // create new asteroid
                 const asteroid = this.getElement()
                 if (asteroid) asteroid.start()
@@ -157,24 +255,22 @@ window.addEventListener("load", function(){
                 this.asteroidTimer += deltaTime
             }
 
-            this.asteroidPool.forEach(asteroid =>{
-               asteroid.draw(context)
-               asteroid.update()
-           })
+            const allObjects = [ this.earth, ...this.asteroidPool, ...this.explosion]
 
-           this.explosion.forEach(explosion =>{
-                explosion.draw(context)
-                explosion.update(deltaTime)
-           })
+            allObjects.forEach(obj => {
+                obj.draw(context)
+                obj.update(deltaTime)
+            })
+           
 
-           console.log(this.explosion)
+
         }
     }
 
     const game = new Game(canvas.width, canvas.height)
 
     let lastTime = 0
-    function animation(timeStamp){
+    function animation(timeStamp) {
         const deltaTime = timeStamp - lastTime
         lastTime = timeStamp
         ctx.clearRect(0, 0, canvas.width, canvas.height)
